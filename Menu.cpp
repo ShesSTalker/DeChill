ea2fabdae2dd4ca6c6207d886d6707c0891196df
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-12 16:51:21 -0300

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -3,8 +3,13 @@
 
 using namespace std;
 
-void Menu::mostrar_menu(){
-    cout <<"------MENU ------"<<endl;
-    cout << ""
-
+void Menu::mostrar_menu()
+{
+    cout <<"--------MENU--------"<< endl <<
+    "1) Listar animales." << endl <<
+    "2) Rescatar animal." << endl <<
+    "3) Buscar animal." << endl <<
+    "4) Ciudar animales." << endl <<
+    "5) Adoptar animal." << endl <<
+    "6) Guardar y salir" << endl << endl; 
 }
