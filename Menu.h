f9c9b094875dba1d47af6ad41489fc15130a64a5
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-11 21:59:06 -0300

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,48 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+class Interfaz{
+    //Metodos
+    public:
+        void cargar_datos();
+        //Pre: -
+        //Pos: Imprime por pantalla un saludo al usuario
+        void bienvenida();
+        //Pre: -
+        //Pos: Imprime por pantalla el menu con las opciones
+        void mostrar_menu();
+        //Pre: -
+        //Pos: Imprime por pantalla los animales de la reserva
+        void mostrar_animales();
+        //Pre: -
+        // Pos: Se agrega un nuevo animal rescatado a la reserva
+        void rescatar_animal();
+        //Pre: -
+        // Pos:  Se buscara el nombre del animal ingresado por el usuario, devolvera true si se encontro el nombre y false en caso contrario 
+        bool buscar_animal();
+        //Pre: -
+        //Pos: Se mostrara por pantalla un menu con las opciones de cuidado disponibles
+        void ciudar_animal();
+        //Pre: El usuario debe ingresar la primera opcion
+        //Pos: Se mostrara los datos de cada animal individualmente y se le preguntara si desaa baniarlo, alimentarlo o saltearlo
+        void elegir_individualmente();
+        //Pre: El usuario debe ingresar la segunda opcion
+        //Pos: El hambre de todos los animales volvera a 0, diciendo su nombre y que comio
+        void alimentar_a_todos();
+        //Pre: El usuario debe ingresar la tercera opcion
+        //Pos: La higiene de los animales subira a 100 y si el animal no quiere banio lo dira por pantalla
+        void baniar_a_todos();
+        //Pre: El usuario debe ingresar la cuarta opcion
+        //Pos: Regresa al menu inicial
+        void regresar_al_inicio(); 
+        //Pre: -
+        //Pos: 
+        void adoptar_animal();
+        //Pre: -
+        //Pos: Se guargan todos los cambios de la reserva en el archivo
+        void guardar_y_salir();
+    
+};
+
+#endif
+
