d136bd14c20c52acefc21e885dff3470e5c59005
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-23 20:29:28 -0300

    implementacion Nodo.h

--- Nodo.h
+++ Nodo.h
@@ -2,29 +2,36 @@
 #define _NODO_H_
 
 template < typename Tipo >
-
 class Nodo
 {
     //Atributos
     private:
-        Tipo animal;
+        Tipo dato;
         Nodo* siguiente;
 
     //Metodos
     public:
-        Nodo (Tipo animal);
+        //PRE: -
+        //POS: Crea un nodo con dato = dato y siguente = 0 (puntero a NULL)
+        Nodo (Tipo dato);
 
+        //PRE: -
+        //POS: El puntero al siguiente nodo apunta a puntero
         void cambiar_siguiente (Nodo* puntero)
         
-        Tipo obtener_nodo();
+        //PRE: -
+        //POS: Devuelve el dato que contiene el nodo
+        Tipo obtener_dato();
 
-        Tipo* obtener_siguiente(); 
+        //PRE: - 
+        //POS: Devuelve el puntero al siguiente nodo
+        Nodo* obtener_siguiente(); 
 };
 
 template < typename Tipo >
-Nodo<Tipo>::Nodo(Tipo animal)
+Nodo<Tipo>::Nodo(Tipo dato)
 {
-    this -> animal = animal;
+    this -> dato = dato;
     this -> siguiente = 0;
 }
 
@@ -35,9 +42,15 @@ void Nodo<Tipo>::cambiar_siguiente(Nodo* puntero)
 }
 
 template < typename Tipo >
-Tipo Nodo<Tipo>::obtener_nodo()
+Tipo Nodo<Tipo>::obtener_dato()
 {
-    return Tipo;
+    return dato;
+}
+
+template < typename Tipo >
+Nodo* Nodo<Tipo>::obtener_siguiente() 
+{
+    return siguiente;
 }
 
 #endif
