e31e2c50e7ddb4e18f5ebf0a16a7c698a27ac2ca
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-22 22:55:23 -0300

    Constantes.h

--- Constantes.h
+++ Constantes.h
@@ -1,6 +1,14 @@
 #ifndef _CONSTANTES_H_
 #define _CONSTANTES_H_
 
+char const perro = 'A';
+char const gato = 'G';
+char const caballo = 'C';
+char const roedor  = 'R';
+char const conejo = 'O';
+char const erizo  = 'E';
+char const lagartija = 'L';
+
 enum opciones_menu_principal {
 LISTAR_ANIMALES = 1,
 RESCATAR_ANIMALES,
