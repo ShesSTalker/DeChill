cac90e32b8c8b2b036721b62d89b0244790a3ba8
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-26 21:31:47 -0300

    Implementancion Perro

--- Perro.cpp
+++ Perro.cpp
@@ -1,5 +1,10 @@
 #include "Perro.h"
 
+bool Perro::requiere_ducha()
+{
+    return true;
+}
+
 string Perro:: que_come()
 {
     return "Huesos";
--- Perro.h
+++ Perro.h
@@ -6,7 +6,13 @@
 class Perro : public Animal
 {
     public:
-    Perro(string nombre ,int edad, char tamanio, char personalidad);
+    
+    //PRE: -
+    //POS: Devuelve verdadero si requiere una ducha y falso si no requiere
+    bool requiere_ducha();
+
+    //PRE: -
+    //POS: Devuelve la comida que come
     string que_come();
 };
 
