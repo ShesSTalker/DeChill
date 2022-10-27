63efcbc9339cf00009832fda2b371ec75b3c3c31
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-26 21:25:19 -0300

    Implementacion Gato

--- Gato.cpp
+++ Gato.cpp
@@ -1,11 +1,12 @@
 #include "Gato.h"
 
+bool Gato:: requiere_ducha()
+{
+    return false;
+}
+
 string Gato:: que_come()
 {
     return "Atun";
 }
 
-bool Gato:: requiere_ducha()
-{
-    return false;
-}
