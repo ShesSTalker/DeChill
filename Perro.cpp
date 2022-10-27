7ab21ae1240aa413f8d0130eb2de4f6f3b2e2fc0
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-27 00:05:40 -0300

    Cambios Perro

--- Perro.cpp
+++ Perro.cpp
@@ -1,6 +1,15 @@
 #include "Perro.h"
 
-Perro::Perro(string nombre, int edad, char tamanio, char personalidad, char especie) : Animal(nombre, edad, tamanio, personalidad, especie) {}
+Perro::Perro(string nombre, int edad, char tamanio, char personalidad)
+{
+    this -> nombre = nombre;
+    this -> hambre = HAMBRE_INICIAL;
+    this -> higiene = HIGIENE_INICIAL;
+    this -> edad = edad;
+    this -> tamanio = tamanio;
+    this -> personalidad = personalidad;
+    this -> especie = PERRO;
+}
 
 bool Perro::requiere_ducha()
 {
