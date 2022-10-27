788d7d0cb904eb6efe622c2fe2cae742125ee8f4
LeandroBrizuela21 <115376419+LeandroBrizuela21@users.noreply.github.com>
2022-10-27 00:01:57 -0300

    Cambios Lagartija

--- Lagartija.h
+++ Lagartija.h
@@ -5,16 +5,21 @@
 
 class Lagartija : public Animal
 {
+    //Atributos
+    private:
+        char especie;
+    
+    //Metodos
     public:
-    Lagartija(string nombre, int edad, char tamanio, char personalidad, char especie);
+        Lagartija(string nombre, int edad, char tamanio, char personalidad);
 
-    //PRE: -
-    //POS: Devuelve verdadero si requiere una ducha y falso si no requiere
-    bool requiere_ducha();
+        //PRE: -
+        //POS: Devuelve verdadero si requiere una ducha y falso si no requiere
+        bool requiere_ducha();
 
-    //PRE: -
-    //POS: Devuelve la comida que come
-    string que_come();
+        //PRE: -
+        //POS: Devuelve la comida que come
+        string que_come();
 };
 
 #endif
